96dcef8feaf4ef27af2d397cff1ee6181c906412
IrohScripts <46058065+IrohScripts@users.noreply.github.com>
2022-02-17 21:45:58 -0500

    Beginning of avatar preview on signup page

--- desktop/client/src/app/components/sign-up/sign-up.component.ts
+++ desktop/client/src/app/components/sign-up/sign-up.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AvatarImportModalComponent } from '../avatar/avatar-import-modal/avatar-import-modal.component';
 import { AvatarSelectionModalComponent } from '../avatar/avatar-selection-modal/avatar-selection-modal.component';
+import { avatars } from '../../interfaces-enums/avatar-list';
+
 
 @Component({
     selector: 'app-sign-up',
@@ -9,6 +11,9 @@ import { AvatarSelectionModalComponent } from '../avatar/avatar-selection-modal/
     styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
+    avatarList: string[] = avatars;
+    avatarFromSelection: number = 0;
+
     constructor(public dialog: MatDialog) {}
 
     openAvatarSelectionModal(): void {
