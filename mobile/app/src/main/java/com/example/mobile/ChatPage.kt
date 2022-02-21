e8d33228a4fbc4c23626d7297a291c64b5b98653
ChaimaSS <chaima.soussi@hotmail.com>
2022-02-21 17:24:49 -0500

    CS ajout correction

--- mobile/app/src/main/java/com/example/mobile/ChatPage.kt
+++ mobile/app/src/main/java/com/example/mobile/ChatPage.kt
@@ -2,10 +2,10 @@ package com.example.mobile
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
+import android.util.Log
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.socket.client.Socket
@@ -22,14 +22,23 @@ class ChatPage : AppCompatActivity() {
     private lateinit var messages : ArrayList<Message>
     private lateinit var user: String
     private lateinit var socket: Socket
+    private lateinit var roomNameView : TextView
+    private lateinit var chatViewOptions: ImageButton
+    private lateinit var roomName : String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_page)
 
+        chatViewOptions = findViewById<ImageButton>(R.id.chatViewOptions)
+        roomName = intent.getStringExtra("roomName").toString()
+        roomNameView = findViewById(R.id.roomName)
+        roomNameView.text = roomName.toString()
+
         rvOutputMsgs = findViewById(R.id.rvOutputMsgs)
         btnSend = findViewById<Button>(R.id.btnSend)
-        leaveChatBtn = findViewById<ImageButton>(R.id.leaveChat)
+        leaveChatBtn = findViewById<ImageButton>(R.id.leaveChatBtn)
         messageText = findViewById<EditText>(R.id.msgText)
         user=intent.getStringExtra("userName").toString()
         messages = ArrayList()
@@ -48,16 +57,19 @@ class ChatPage : AppCompatActivity() {
         btnSend.setOnClickListener{
             if(messageText.text.length > 0) {
                 if(!messageText.text.isNullOrBlank() ) {
-                    jo.put("userName", user)
-                    jo.put("message", messageText.text.toString())
+                    var messageData : JSONObject = JSONObject()
+                    messageData.put("userName", user)
+                    messageData.put("message", messageText.text.toString())
                     val current = LocalDateTime.now()
                     val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
                     val formatted = current.format(formatter)
-                    jo.put("time", formatted)
-                    socket.emit("message", jo)
+                    messageData.put("time", formatted)
+                    messageData.put("room", roomName)
+                    socket.emit("message", messageData)
                 }
             }
         }
+
 
         leaveChatBtn.setOnClickListener {
             leaveChat()
@@ -66,13 +78,14 @@ class ChatPage : AppCompatActivity() {
         socket.on("message"){ args ->
 
             if(args[0] != null){
-
-                jo = args[0] as JSONObject
-                val message = jo.get("message") as String
-                val user = jo.get("userName") as String
-                val time = jo.get("time") as String
+                var messageData : JSONObject = JSONObject()
+                messageData = args[0] as JSONObject
+                val message = messageData.get("message") as String
+                val user = messageData.get("userName") as String
+                val time = messageData.get("time") as String
+                val room = messageData.get("room") as String
                 runOnUiThread{
-                    val msg = Message(message, user, time)
+                    val msg = Message(message, user, time, room)
                     msgAdapter.addMsg(msg)
                     msgAdapter.notifyItemInserted((rvOutputMsgs.adapter as MessageAdapter).itemCount)
                     rvOutputMsgs.scrollToPosition((rvOutputMsgs.adapter as MessageAdapter).itemCount-1)
@@ -80,18 +93,80 @@ class ChatPage : AppCompatActivity() {
                 }
             }
         }
+
+        socket.on("newUserToChatRoom"){ args ->
+            if(args[0] != null){
+                var messageData : JSONObject = JSONObject()
+                messageData = args[0] as JSONObject
+                val user = messageData.get("userName") as String
+                val room = messageData.get("room") as String
+                runOnUiThread{
+                    val msg = Message(null, user, null, room)
+                    msgAdapter.addMsg(msg)
+                    msgAdapter.notifyItemInserted((rvOutputMsgs.adapter as MessageAdapter).itemCount)
+                    rvOutputMsgs.scrollToPosition((rvOutputMsgs.adapter as MessageAdapter).itemCount-1)
+                    messageText.text.clear()
+                }
+            }
+        }
+
+        //handle popup menu options
+        chatViewOptions.setOnClickListener {
+            val popupMenu = PopupMenu(
+                this,
+                chatViewOptions
+            )
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                //get id of the item clicked and handle clicks
+                when (menuItem.itemId) {
+                    R.id.menu_members -> {
+                        Toast.makeText(this, "Membres", Toast.LENGTH_LONG).show()
+                        true
+                    }
+                    R.id.menu_leaveChat -> {
+                        userLeftChat()
+                        Toast.makeText(this, "Quitter", Toast.LENGTH_LONG).show()
+                        true
+                    }
+                    R.id.menu_deleteChat -> {
+                        Toast.makeText(this, "Supprimer", Toast.LENGTH_LONG).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popupMenu.inflate(R.menu.chatpage_options_menu)
+
+            //to show icons for menu
+            try {
+                val fieldMPopup = PopupMenu::class.java.getDeclaredField("mPopup")
+                fieldMPopup.isAccessible = true
+                val mPopup = fieldMPopup.get(popupMenu)
+                mPopup.javaClass.getDeclaredMethod("setForceShowIcon", Boolean::class.java).invoke(mPopup, true)
+            } catch (e: Exception) {
+                Log.e("ChatPage", "Error showing menu icons", e)
+            } finally {
+                popupMenu.show()
+            }
+        }
     }
 
     fun leaveChat(){
-        val intent = Intent(this, MainActivity::class.java)
-        socket.emit("disconnectUser", user.toString())
-        SocketHandler.closeConnection()
+        val intent = Intent(this, ChatRooms::class.java)
         startActivity(intent)
     }
 
-    override fun onStop() {
+    /*override fun onStop() {
         super.onStop()
         leaveChat()
+    }*/
+    fun userLeftChat () {
+        var roomData : JSONObject = JSONObject()
+        roomData.put("userName", user)
+        roomData.put("room", roomName)
+        socket.emit("leaveRoom", roomData)
+        leaveChat()
     }
+
 }
 
--- mobile/app/src/main/java/com/example/mobile/Message.kt
+++ mobile/app/src/main/java/com/example/mobile/Message.kt
@@ -1,15 +1,17 @@
 package com.example.mobile
 
- class Message {
-     var msgText: String ? = null
-     var user: String ? = null
-     var time: String ? = null
+class Message {
+    var msgText: String ? = null
+    var user: String ? = null
+    var time: String ? = null
+    var room: String ? = null
 
-     constructor(){}
+    constructor(){}
 
-     constructor(message:String?, senderId : String?, sendTime : String?){
-         this.msgText = message
-         this.user = senderId
-         this.time = sendTime
-     }
- }
+    constructor(message:String?, senderId : String?, sendTime : String?, roomName: String?){
+        this.msgText = message
+        this.user = senderId
+        this.time = sendTime
+        this.room = roomName
+    }
+}
