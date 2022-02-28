d57a70c795926822df70a27e54ea23bab143a4a5
IrohScripts <46058065+IrohScripts@users.noreply.github.com>
2022-02-28 14:55:24 -0500

    Le bon stroke est selectionné quand on click avec l'outil selection

--- desktop/client/src/app/services/editor/tools/selection/selection.service.ts
+++ desktop/client/src/app/services/editor/tools/selection/selection.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Stroke } from '@app/classes/strokes/stroke';
 import { Tool } from '@app/classes/tool';
+import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/editor/drawing/drawing.service';
 
 @Injectable({
@@ -8,6 +9,7 @@ import { DrawingService } from '@app/services/editor/drawing/drawing.service';
 })
 export class SelectionService extends Tool {
   strokes: Stroke[];
+  selectedStroke: Stroke;
 
   constructor(drawingService: DrawingService) {
     super(drawingService);
@@ -16,5 +18,35 @@ export class SelectionService extends Tool {
 
   addStroke(stroke: Stroke): void {
     this.strokes.push(stroke);
+  }
+
+  onMouseClick(event: MouseEvent): void {
+    if (this.setSelectedStroke(this.getPositionFromMouse(event))) {
+      console.log('Stroke selected: ', this.selectedStroke);
+
+    } else {
+      console.log('no stroke in bounds');
+    }
+  }
+
+  private setSelectedStroke(clickedPos: Vec2): boolean {
+    for (let i = this.strokes.length - 1; i >= 0; i--) {
+      if (this.isInBounds(this.strokes[i].boundingPoints, clickedPos)) {
+        this.selectedStroke = this.strokes[i];
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private isInBounds(bounds: Vec2[], pointToCheck: Vec2): boolean {
+    const topLeftBound = bounds[0];
+    const bottomRightBound = bounds[1];
+    return (
+      pointToCheck.x >= topLeftBound.x &&
+      pointToCheck.x <= bottomRightBound.x &&
+      pointToCheck.y >= topLeftBound.y &&
+      pointToCheck.y <= bottomRightBound.y
+    );
   }
 }
