0701f6e496564a8a2eac6c54e69a5897d620eddb
Noursen12 <houidhek.noursen@gmail.com>
2022-03-06 14:54:13 -0500

    ajout radio button

--- mobile/app/src/main/java/com/example/mobile/CreateAlbumPopUp.kt
+++ mobile/app/src/main/java/com/example/mobile/CreateAlbumPopUp.kt
@@ -1,60 +1,49 @@
 package com.example.mobile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.fragment_create_album_pop_up.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CreateAlbumPopUp.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CreateAlbumPopUp : DialogFragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var radioButton: RadioButton
+    private lateinit var submitButton: Button
+    private lateinit var cancelButton: Button
+    private lateinit var textViewAlbumName: TextView
+    private lateinit var textViewVisibility: TextView
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_album_pop_up, container, false)
+
+        var rootView: View= inflater.inflate(R.layout.fragment_create_album_pop_up, container, false)
+        radioGroup=rootView.findViewById(R.id.radioGroup)
+        textViewAlbumName=rootView.findViewById(R.id.text_view_albumName)
+        textViewVisibility=rootView.findViewById(R.id.text_view_visibility)
+        submitButton= rootView.findViewById(R.id.submitBtn)
+        cancelButton=rootView.findViewById(R.id.cancelBtn)
+
+        cancelButton.setOnClickListener(){
+            dismiss()
+        }
+
+        submitButton.setOnClickListener(){
+            val selectedID=radioGroup.checkedRadioButtonId
+            val radio= rootView.findViewById<RadioButton>(selectedID)
+
+            var result=radio.text.toString()
+            Toast.makeText(context, "choix d'accès est $result", Toast.LENGTH_LONG)
+
+        }
+        
+        return rootView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateAlbumPopUp.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CreateAlbumPopUp().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
