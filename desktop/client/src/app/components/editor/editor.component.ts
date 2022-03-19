99f61cbaac6118b171f80d73f8824d93359db47d
IrohScripts <46058065+IrohScripts@users.noreply.github.com>
2022-03-19 19:18:20 -0400

    bug fix drawing creation dialog

--- desktop/client/src/app/components/editor/editor.component.ts
+++ desktop/client/src/app/components/editor/editor.component.ts
@@ -1,7 +1,9 @@
 import { AfterViewInit, Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ToolList } from '@app/interfaces-enums/tool-list';
 import { ToolManagerService } from '@app/services/editor/tools/tool-manager.service';
 import { CollaborationService } from '@app/services/collaboration/collaboration.service';
+import { CreateDrawingDialogComponent } from './create-drawing-dialog/create-drawing-dialog.component';
 
 @Component({
     selector: 'app-editor',
@@ -10,7 +12,14 @@ import { CollaborationService } from '@app/services/collaboration/collaboration.
 })
 export class EditorComponent implements AfterViewInit {
     ToolList: typeof ToolList = ToolList;
-    constructor(public toolManagerService: ToolManagerService, private collaborationService : CollaborationService) {}
+    constructor(public toolManagerService: ToolManagerService, private collaborationService : CollaborationService, public dialog: MatDialog) {}
+
+    ngOnInit(): void {
+        this.dialog.open(CreateDrawingDialogComponent, {
+          width: "50%",
+          disableClose: true
+        });
+    }
 
     ngAfterViewInit(): void {
         this.collaborationService.enterCollaboration();
