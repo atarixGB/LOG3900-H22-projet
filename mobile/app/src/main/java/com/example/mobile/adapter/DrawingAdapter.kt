02938882493aec35a68ed32ebbb86eda294f9d65
Noursen12 <houidhek.noursen@gmail.com>
2022-03-25 15:54:24 -0400

    ajout de liste deroulante dans chaque drawing

--- mobile/app/src/main/java/com/example/mobile/adapter/DrawingAdapter.kt
+++ mobile/app/src/main/java/com/example/mobile/adapter/DrawingAdapter.kt
@@ -1,20 +1,28 @@
 package com.example.mobile.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobile.Interface.IDrawing
 import com.example.mobile.R
+import com.example.mobile.activity.albums.Albums
 import com.example.mobile.bitmapDecoder
+
+
+import com.example.mobile.popup.DrawingNameModificationPopUp
 import kotlinx.android.synthetic.main.item_drawing.view.*
 import java.util.ArrayList
 
-class DrawingAdapter (val context: Context?, var drawings: ArrayList<IDrawing>, val user: String) : RecyclerView.Adapter<DrawingAdapter.DrawingViewHolder>() {
+class DrawingAdapter (val context: Context?, var drawings: ArrayList<IDrawing>, val user: String) : RecyclerView.Adapter<DrawingAdapter.DrawingViewHolder>(),DrawingNameModificationPopUp.DialogListener {
 
     private var listener: DrawingAdapterListener = context as DrawingAdapterListener
+    private lateinit var dialogEditDrawingName: DrawingNameModificationPopUp
 //    private var alreadyLiked: Boolean = false
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DrawingViewHolder {
@@ -55,6 +63,53 @@ class DrawingAdapter (val context: Context?, var drawings: ArrayList<IDrawing>, 
                 listener.drawingAdapterListener(drawingName.text.toString())
             }
 
+            drawingViewOptions.setOnClickListener {
+                //getAlbumParameters(albumName) //pour avoir les parametres d'album a jour
+                val popupMenu = PopupMenu(
+                    context,
+                    drawingViewOptions
+                )
+                popupMenu.setOnMenuItemClickListener { menuItem ->
+                    //get id of the item clicked and handle clicks
+                    when (menuItem.itemId) {
+                        R.id.menu_editDrawingParameters -> {
+                            //ouvrir pop up modification nom album
+                            dialogEditDrawingName= DrawingNameModificationPopUp(currentDrawing.name)
+                            dialogEditDrawingName.showsDialog
+                            true
+                        }
+                        R.id.menu_changeAlbum -> {
+                        true
+                        }
+                        R.id.menu_deleteDrawing -> {
+                            deleteDrawing(currentDrawing._id!!)
+                            true
+                        }
+                        else -> false
+                    }
+                }
+
+                popupMenu.inflate(R.menu.drawing_options_menu)
+
+                if (user != currentDrawing.owner) {
+                    popupMenu.menu.findItem(R.id.menu_editDrawingParameters).isVisible = false
+                    popupMenu.menu.findItem(R.id.menu_changeAlbum).isVisible = false
+                    popupMenu.menu.findItem(R.id.menu_deleteDrawing).isVisible=false
+                }
+
+                //to show icons for menu
+                try {
+                    val fieldMPopup = PopupMenu::class.java.getDeclaredField("mPopup")
+                    fieldMPopup.isAccessible = true
+                    val mPopup = fieldMPopup.get(popupMenu)
+                    mPopup.javaClass.getDeclaredMethod("setForceShowIcon", Boolean::class.java).invoke(mPopup, true)
+                } catch (e: Exception) {
+                    Log.e("DrawingAdapter", "Error showing menu icons", e)
+                } finally {
+                    popupMenu.show()
+                }
+            }
+
             likeBtn.setOnClickListener {
                 if (!likes.contains(user)){
                     listener.addLikeToDrawingAdapterListener(currentDrawing._id!!)
@@ -89,4 +144,12 @@ class DrawingAdapter (val context: Context?, var drawings: ArrayList<IDrawing>, 
 
 
     class DrawingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private fun deleteDrawing(drawingId: String){
+
+    }
+
+    override fun popUpListener(drawingName: String) {
+
+    }
 }
---
+++ mobile/app/src/main/java/com/example/mobile/popup/DrawingNameModificationPopUp.kt
@@ -0,0 +1,63 @@
+package com.example.mobile.popup
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.example.mobile.R
+
+
+class DrawingNameModificationPopUp (val oldDrawingName: String) : DialogFragment(){
+    private lateinit var listener: DialogListener
+    private lateinit var submitButton: Button
+    private lateinit var cancelButton: Button
+    private lateinit var drawingName: EditText
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView: View = inflater.inflate(R.layout.fragment_drawing_name_modification_pop_up, container, false)
+        drawingName=rootView.findViewById(R.id.modifiedDrawingName)
+        drawingName.setText(oldDrawingName)
+
+        submitButton= rootView.findViewById(R.id.modifyAlbumBtn)
+        cancelButton=rootView.findViewById(R.id.cancelBtn)
+
+        cancelButton.setOnClickListener(){
+            dismiss()
+        }
+
+        submitButton.setOnClickListener{
+
+            var modifiedName= drawingName.text.toString()
+
+            listener.popUpListener(modifiedName)
+
+            dismiss()
+
+        }
+
+        return rootView
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = try {
+            context as DrawingNameModificationPopUp.DialogListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException(
+                context.toString() + "must implement DialogListener"
+            )
+        }
+    }
+    public interface DialogListener {
+        fun popUpListener(drawingName: String)
+    }
+}
