5017170fafb10ea813f2baee08045a486c6a24c3
Mikael Gilbert <leakim_gilbert9@hotmail.com>
2022-03-26 13:08:19 -0400

    Ajout interface IUser

---
+++ desktop/client/src/app/interfaces-enums/IUser.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  _id?: any,
+  identifier: string,
+  password: string,
+  salt?: string
+  avatar: any,
+  email: string,
+  description?: string,
+  favoriteDrawingsID?: string[];
+}
