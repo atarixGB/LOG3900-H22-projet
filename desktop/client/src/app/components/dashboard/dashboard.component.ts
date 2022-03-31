dc5aa43648c6d7923382128d4b4e0586b3d85ddc
IrohScripts <46058065+IrohScripts@users.noreply.github.com>
2022-03-31 00:09:22 -0400

    Ajout du Ui du dashboard pour les stories

--- desktop/client/src/app/components/dashboard/dashboard.component.ts
+++ desktop/client/src/app/components/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { fadeInAnimation } from '@app/constants/animations';
 import { CreateDrawingDialogComponent } from '@app/components/editor/create-drawing-dialog/create-drawing-dialog.component';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IUser } from '@app/interfaces-enums/IUser';
+import { avatars } from '@app/interfaces-enums/avatar-list';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,10 +14,23 @@ import { ActivatedRoute, Router } from '@angular/router';
   host: { '[@fadeInAnimation]': '' }
 })
 export class DashboardComponent implements OnInit {
+  users: IUser[];
 
-  constructor(public dialog: MatDialog, private router: Router, private route: ActivatedRoute) { }
+  constructor(public dialog: MatDialog, private router: Router, private route: ActivatedRoute) { 
+    this.users = [];
+    let templateUser: IUser = {
+      identifier: 'a',
+      password: 'a',
+      avatar: avatars[0],
+      email: 'a',
+    }
+    for(let i = 0; i < 3; i++) {
+      this.users.push(templateUser);
+    }
+  }
 
   ngOnInit(): void {
+    
   }
 
   displayCreateNewDrawingDialog(): void {
--- desktop/client/src/app/interfaces-enums/IUser.ts
+++ desktop/client/src/app/interfaces-enums/IUser.ts
@@ -2,7 +2,7 @@ export interface IUser {
   _id?: any,
   identifier: string,
   password: string,
-  salt?: string
+  salt?: string,
   avatar: any,
   email: string,
   description?: string,
