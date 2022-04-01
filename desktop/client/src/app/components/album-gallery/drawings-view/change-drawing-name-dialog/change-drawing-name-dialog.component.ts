f8f00e9a23db449f1f8dd6dc9ca1364440208611
Mikael Gilbert <leakim_gilbert9@hotmail.com>
2022-04-01 11:12:32 -0400

    Remise de l'url de API sur localhost + Ajout de validation lors de la modification du nom d'un album et d'un dessin

--- desktop/client/src/app/components/album-gallery/drawings-view/change-drawing-name-dialog/change-drawing-name-dialog.component.ts
+++ desktop/client/src/app/components/album-gallery/drawings-view/change-drawing-name-dialog/change-drawing-name-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { IDrawing } from '@app/interfaces-enums/IDrawing';
+import { NAME_MAX_LENGTH } from '@app/constants/constants';
 import { AlbumGalleryService } from '@app/services/album-gallery/album-gallery.service';
 
 @Component({
@@ -8,18 +9,21 @@ import { AlbumGalleryService } from '@app/services/album-gallery/album-gallery.s
   templateUrl: './change-drawing-name-dialog.component.html',
   styleUrls: ['./change-drawing-name-dialog.component.scss']
 })
-export class ChangeDrawingNameDialogComponent implements OnInit {
+export class ChangeDrawingNameDialogComponent {
   newDrawingName: string;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: IDrawing , public albumGalleryService: AlbumGalleryService) { }
 
-  ngOnInit(): void {
-    console.log(this.data);
-
+  changeDrawingName(): void {
+    if (this.isValidInput(this.newDrawingName) && this.newDrawingName.length < NAME_MAX_LENGTH) {
+      this.albumGalleryService.changeDrawingName(this.data, this.newDrawingName);
+    } else {
+      alert("Le nouveau nom du dessin doit posséder entre 1 et 40 caractères inclusivement")
+    }
   }
 
-  changeDrawingName(): void {
-    this.albumGalleryService.changeDrawingName(this.data, this.newDrawingName);
+  private isValidInput(str: string): boolean {
+    return !(str === null || str.match(/^ *$/) !== null);
   }
 
 }
