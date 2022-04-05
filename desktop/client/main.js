863f20bb1bca3a4e513373be92a2feafae727386
Mikael Gilbert <leakim_gilbert9@hotmail.com>
2022-04-05 13:16:15 -0400

    Création d'une nouvelle fenêtre indépendante pour le chat, ajout d'un icône pour l'app, taille de fenêtre fixe (non redimensionnable)

--- desktop/client/main.js
+++ desktop/client/main.js
@@ -2,13 +2,22 @@ const { app, BrowserWindow } = require('electron');
 const url = require('url');
 const path = require('path');
 
-let appWindow;
+let appWindow, chatWindow;
+const APP_ICON = path.join(__dirname, '/dist/client/assets/PolyGramLogo.png');
 
 function initWindow() {
+    /********** MAIN APP WINDOW **********/
     appWindow = new BrowserWindow({
+        icon: APP_ICON,
         // fullscreen: true,
-        height: 800,
-        width: 1000,
+        height: 1000,
+        // minHeight: 800,
+        // maxHeight: 1080,
+        width: 1400,
+        // minWidth: 1000,
+        // maxWidth: 1920,
+        resizable: false,
+
         webPreferences: {
             nodeIntegration: true,
         },
@@ -22,13 +31,36 @@ function initWindow() {
             slashes: true,
         }),
     );
-    //appWindow.setMenuBarVisibility(false)
+    appWindow.setMenuBarVisibility(false);
 
     // Initialize the DevTools.
     // appWindow.webContents.openDevTools()
 
     appWindow.on('closed', function () {
         appWindow = null;
+        app.quit();
+    });
+
+    /********** CHAT WINDOW **********/
+    chatWindow = new BrowserWindow({
+        height: 800,
+        width: 1000,
+        center: true,
+        // show: false,
+    });
+
+    chatWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/dist/client/index.html`),
+            protocol: 'file:',
+            slashes: true,
+            hash: '/chatroom'
+        }),
+    );
+    chatWindow.setMenuBarVisibility(false);
+
+    chatWindow.on('closed', function () {
+        chatWindow = null;
     });
 }
 
