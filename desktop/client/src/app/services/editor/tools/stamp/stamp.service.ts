40cb92863cec53c157013dcc0f3dc971ea2e3c1b
IrohScripts <46058065+IrohScripts@users.noreply.github.com>
2022-04-09 20:38:34 -0400

    Rotation correction

--- desktop/client/src/app/services/editor/tools/stamp/stamp.service.ts
+++ desktop/client/src/app/services/editor/tools/stamp/stamp.service.ts
@@ -3,7 +3,6 @@ import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { MOST_LIKED } from '@app/constants/badges';
 import { MouseButton } from '@app/constants/constants';
-import { DEAD, GLASSES, HAPPY, SAD, SURPRISED } from '@app/constants/stamp-data';
 import { StampList } from '@app/interfaces-enums/stamp-list';
 import { DrawingService } from '@app/services/editor/drawing/drawing.service';
 import { Subject } from 'rxjs';
@@ -11,7 +10,6 @@ import { Subject } from 'rxjs';
 const SCALE_FACTOR_STAMP = 1;
 const ROTATION_STEP_STAMP = 15;
 const MAX_ANGLE = 360;
-// const ANGLE_HALF_TURN = 180;
 const SIZE_STAMP = 24;
 @Injectable({
     providedIn: 'root',
@@ -20,7 +18,7 @@ export class StampService extends Tool {
     imageCoords: Vec2;
     currentStamp: string;
     selectStamp: StampList;
-    imageSrc: string;
+    img: HTMLImageElement;
     size: number;
     resizeFactor: number;
     color: string;
@@ -35,23 +33,11 @@ export class StampService extends Tool {
 
     constructor(drawingService: DrawingService) {
         super(drawingService);
+        this.img = new Image(this.size, this.size);
+        this.img.src = MOST_LIKED;
 
-        this.stampBindings = new Map<StampList, string>();
-        this.stampBindings
-            .set(StampList.Surprised, 'surprised')
-            .set(StampList.Happy, 'happy')
-            .set(StampList.Sad, 'sad')
-            .set(StampList.Glasses, 'glasses')
-            .set(StampList.Dead, 'dead');
-
-        this.srcBinding = new Map<string, string>();
-        this.srcBinding.set('surprised', SURPRISED).set('happy', HAPPY).set('sad', SAD).set('glasses', GLASSES).set('dead', DEAD);
-
-        this.currentStamp = 'happy';
-        this.selectStamp = StampList.Happy;
         this.size = SIZE_STAMP;
         this.angle = 0;
-        this.isKeyAltDown = false;
         this.resizeFactor = SCALE_FACTOR_STAMP;
     }
 
@@ -63,31 +49,18 @@ export class StampService extends Tool {
         }
     }
 
-    handleKeyDown(event: KeyboardEvent): void {
-        if (event.key === 'Alt') {
-            event.preventDefault();
-            this.isKeyAltDown = true;
-        }
-    }
-
-    handleKeyUp(event: KeyboardEvent): void {
-        if (event.key === 'Alt') {
-            this.isKeyAltDown = false;
-        }
-    }
-
     onMouseMove(event: MouseEvent): void {
         this.mouseEvent = event;
+        this.imageCoords = this.getPositionFromMouse(event);
+        this.drawingService.clearCanvas(this.drawingService.previewCtx);
         this.previewCursor(this.getPositionFromMouse(event));
     }
 
     onWheelEvent(event: WheelEvent): void {
         let rotationStep = ROTATION_STEP_STAMP;
-        if (this.isKeyAltDown) {
-            rotationStep = 1;
-        }
         this.changeAngle(this.angle - (event.deltaY / Math.abs(event.deltaY)) * rotationStep);
-        this.onMouseMove(this.mouseEvent);
+        this.drawingService.clearCanvas(this.drawingService.previewCtx);
+        this.previewCursor(this.getPositionFromMouse(event));
     }
 
     changeStamp(): void {
@@ -95,6 +68,7 @@ export class StampService extends Tool {
             this.currentStamp = this.stampBindings.get(this.selectStamp)!;
         }
     }
+
     private changeAngle(newAngle: number): void {
         newAngle %= MAX_ANGLE;
 
@@ -105,58 +79,24 @@ export class StampService extends Tool {
         this.angle = newAngle;
         this.angleObservable.next(this.angle);
     }
+    
     private drawStamp(event: Vec2): void {
-        /*if (this.srcBinding.has(this.currentStamp)) {
-            this.imageSrc = this.srcBinding.get(this.currentStamp)!;
-        }
-
-        this.size = 24 * this.resizeFactor;
-
-        let img = new Image(this.size, this.size);
-        img.src = MOST_LIKED;
-
-        const center: Vec2 = { x: event.x, y: event.y };
-
-        this.drawingService.baseCtx.lineWidth = 1;
-
-        this.drawingService.baseCtx.translate(center.x, center.y);
-
-        this.drawingService.baseCtx.scale(this.resizeFactor, this.resizeFactor);
-        this.drawingService.baseCtx.translate(-this.size / 2, -this.size / 2);
-
-        this.drawingService.baseCtx.rotate(-((this.angle * Math.PI) / ANGLE_HALF_TURN));
-        this.drawingService.baseCtx.translate(-this.size / 2, -this.size / 2);
-
-        console.log(this.resizeFactor);
-        
-
-        this.drawingService.baseCtx.drawImage(
-            img,
-            0,
-            0,
-            this.size,
-            this.size,
-        );
-
-        this.drawingService.baseCtx.setTransform(1, 0, 0, 1, 0, 0);*/
-        let img = new Image(this.size, this.size);
-        img.src = MOST_LIKED;
-
+        this.drawingService.clearCanvas(this.drawingService.previewCtx);
         const realStampWidth = this.size * this.resizeFactor;
         const realStampHeight = this.size * this.resizeFactor;
         let ctx = this.drawingService.baseCtx;
         ctx.save();
         ctx.translate(this.imageCoords.x + realStampWidth / 2, this.imageCoords.y + realStampHeight / 2);
-        ctx.rotate(this.angle);
+        ctx.rotate(this.angle * Math.PI / 180);
         ctx.translate(-this.imageCoords.x - realStampWidth / 2, -this.imageCoords.y - realStampHeight / 2);
         ctx.drawImage(
-            img,
+            this.img,
             this.imageCoords.x -
                 (realStampWidth / 2) *
-                    (Math.cos(this.angle) + Math.sin(this.angle)),
+                    (Math.cos(this.angle * Math.PI / 180) + Math.sin(this.angle * Math.PI / 180)),
             this.imageCoords.y -
                 (realStampHeight / 2) *
-                    (Math.cos(this.angle) - Math.sin(this.angle)),
+                    (Math.cos(this.angle * Math.PI / 180) - Math.sin(this.angle * Math.PI / 180)),
             realStampWidth,
             realStampHeight,
         );
@@ -164,25 +104,24 @@ export class StampService extends Tool {
     }
 
     private previewCursor(event: Vec2): void {
-        /*this.drawingService.clearCanvas(this.drawingService.cursorCtx);
-
-        if (this.srcBinding.has(this.currentStamp)) {
-            this.imageSrc = this.srcBinding.get(this.currentStamp)!;
-        }
-
-        const path = new Path2D(this.imageSrc);
-        const center: Vec2 = { x: event.x + this.size / 2, y: event.y + this.size / 2 };
-
-        this.drawingService.cursorCtx.translate(center.x, center.y);
-
-        this.drawingService.cursorCtx.scale(this.resizeFactor, this.resizeFactor);
-        this.drawingService.cursorCtx.translate(-this.size / 2, -this.size / 2);
-
-        this.drawingService.cursorCtx.rotate(-((this.angle * Math.PI) / ANGLE_HALF_TURN));
-        this.drawingService.cursorCtx.translate(-this.size / 2, -this.size / 2);
-
-        this.drawingService.cursorCtx.stroke(path);
-        this.drawingService.cursorCtx.fill(path);
-        this.drawingService.cursorCtx.setTransform(1, 0, 0, 1, 0, 0);*/
+        const realStampWidth = this.size * this.resizeFactor;
+        const realStampHeight = this.size * this.resizeFactor;
+        let ctx = this.drawingService.previewCtx;
+        ctx.save();
+        ctx.translate(this.imageCoords.x + realStampWidth / 2, this.imageCoords.y + realStampHeight / 2);
+        ctx.rotate(this.angle * Math.PI / 180);
+        ctx.translate(-this.imageCoords.x - realStampWidth / 2, -this.imageCoords.y - realStampHeight / 2);
+        ctx.drawImage(
+            this.img,
+            this.imageCoords.x -
+                (realStampWidth / 2) *
+                    (Math.cos(this.angle * Math.PI / 180) + Math.sin(this.angle * Math.PI / 180)),
+            this.imageCoords.y -
+                (realStampHeight / 2) *
+                    (Math.cos(this.angle * Math.PI / 180) - Math.sin(this.angle * Math.PI / 180)),
+            realStampWidth,
+            realStampHeight,
+        );
+        ctx.restore();
     }
 }
